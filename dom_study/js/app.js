cf01cb30fb3650a862cf3dcd76d6e0fd3df76939
dongyoon7212 <dongyoon7212@naver.com>
2025-07-22 21:49:04 +0900

    Feat: dom

--- dom_study/js/app.js
+++ dom_study/js/app.js
@@ -15,6 +15,7 @@ function app() {
             <h2>작성일: ${appInfo.date}</h2>
             <h3>작성자: ${appInfo.author}</h3>
             ${studentRegister()}
+            <ul class="student-list"></ul>
         </div>
     `;
 }
