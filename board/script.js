e1558f914973c6c676148a19ad793cd54de2b819
dongyoon7212 <dongyoon7212@naver.com>
2025-08-01 21:37:22 +0900

    feat: board

--- board/script.js
+++ board/script.js
@@ -3,6 +3,7 @@ const API_BASE_URL = "http://localhost:8080";
 //메뉴
 const navSignin = document.querySelector("#nav-signin");
 const navSignup = document.querySelector("#nav-signup");
+const navPassword = document.querySelector("#nav-password");
 const navLogout = document.querySelector("#nav-logout");
 const navBoard = document.querySelector("#nav-board");
 const navWrite = document.querySelector("#nav-write");
@@ -11,6 +12,7 @@ const navWrite = document.querySelector("#nav-write");
 //페이지
 const pageSignin = document.querySelector("#page-signin");
 const pageSignup = document.querySelector("#page-signup");
+const pagePassword = document.querySelector("#page-password");
 const pageBoard = document.querySelector("#page-board");
 const pageWrite = document.querySelector("#page-write");
 const pageDetail = document.querySelector("#page-detail");
@@ -18,6 +20,7 @@ const pageDetail = document.querySelector("#page-detail");
 //로그인 및 회원가입 폼
 const signupForm = document.querySelector("#signup-form");
 const signinForm = document.querySelector("#signin-form");
+const passwordForm = document.querySelector("#password-form");
 
 //게시판 목록
 const boardList = document.querySelector("#board-list");
@@ -59,6 +62,8 @@ function getPayload() {
 
 // 페이지 전환 함수
 function changePages(pageElement) {
+	signinForm.reset();
+	signupForm.reset();
 	const pages = document.querySelectorAll(".page");
 	pages.forEach((page) => {
 		page.classList.remove("active");
@@ -296,6 +301,69 @@ async function signupHandler(event) {
 	}
 }
 
+async function changePassword(event) {
+	event.preventDefault();
+
+	const oldPasswordInput = document.querySelector("#old-password");
+	const newPasswordInput = document.querySelector("#new-password");
+	const newConfirmPasswordInput = document.querySelector(
+		"#new-confirm-password"
+	);
+
+	const accessToken = localStorage.getItem("AccessToken");
+
+	const userInfo = getPayload(accessToken);
+
+	if (!accessToken) {
+		alert("잘못된 접근입니다.");
+		changePages(pageSignin);
+		return;
+	}
+
+	if (newPasswordInput.value !== newConfirmPasswordInput.value) {
+		alert("비밀번호가 일치하지 않습니다.");
+		return;
+	}
+
+	const passwordData = {
+		userId: userInfo.jti,
+		oldPassword: oldPasswordInput.value,
+		newPassword: newPasswordInput.value,
+	};
+
+	if (!passwordData.oldPassword || !passwordData.newPassword) {
+		alert("모든 항목을 입력해주세요.");
+		return;
+	}
+
+	try {
+		const response = await fetch(
+			`${API_BASE_URL}/account/change/password`,
+			{
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					Authorization: `Bearer ${accessToken}`,
+				},
+				body: JSON.stringify(passwordData),
+			}
+		);
+
+		const responseData = await response.json();
+
+		if (responseData.status !== "success") {
+			alert(responseData.message);
+		} else {
+			alert(responseData.message);
+			localStorage.removeItem("AccessToken");
+			location.reload(true);
+		}
+	} catch (error) {
+		console.log(error);
+		alert("비밀번호 변경 요청에 오류가 발생했습니다.");
+	}
+}
+
 navSignin.addEventListener("click", () => {
 	changePages(pageSignin);
 });
@@ -310,6 +378,9 @@ navLogout.addEventListener("click", () => {
 		return;
 	}
 });
+navPassword.addEventListener("click", () => {
+	changePages(pagePassword);
+});
 navBoard.addEventListener("click", renderBoard);
 navWrite.addEventListener("click", () => {
 	changePages(pageWrite);
@@ -320,6 +391,7 @@ backBtn.addEventListener("click", renderBoard);
 signupForm.addEventListener("submit", signupHandler);
 signinForm.addEventListener("submit", signinHandler);
 writeForm.addEventListener("submit", addBoard);
+passwordForm.addEventListener("submit", changePassword);
 
 //HTML 문서가 완전히 로드되고 파싱되었을때
 document.addEventListener("DOMContentLoaded", async () => {
@@ -331,6 +403,14 @@ document.addEventListener("DOMContentLoaded", async () => {
 		await renderBoard();
 	} else {
 		navLogout.style.display = "none";
+		navPassword.style.display = "none";
 		changePages(pageSignin);
 	}
 });
+
+//비밀번호 변경
+//메뉴 버튼 만들고 액세스 토큰에 따라서 보이고 안보이고 처리
+//요청 보낼때 userId, oldPassword, newPassword body로 요청 보내기
+//accesstoken도 같이 헤더에 포함해서
+//새로운 비밀번호 입력은 두번 받아서 두개의 값이 같은지 확인 후 요청 처리
+//비밀번호가 변경되면 로그아웃 처리하고 로그인페이지로 전환
